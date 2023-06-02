c2353bc728c1516699e70ec2f09bb7d16fc4e65f
Amanda <amandaoliveirapolari@gmail.com>
2023-06-02 16:08:31 -0300

    Prática 1 - ok!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styled, { createGlobalStyle } from 'styled-components';
 import TelaLogin from './components/TelaLogin/TelaLogin';
 import TelaCadastro from './components/TelaCadastro/TelaCadastro';
 import TelaPrincipal from './components/TelaPrincipal/TelaPrincipal.js';
+import { useState } from 'react';
 
 const GlobalStyled = createGlobalStyle`
   html {
@@ -13,23 +14,101 @@ const GlobalStyled = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
   }
+
+  body {
+    background-color: lightslategray;
+  }
 `;
 const MainContainer = styled.main`
     height: 100vh;
 `;
 
 function App() {
-    return (
-        <MainContainer>
-            <GlobalStyled />
+    // DEFININDO ESTADOS:
+    const [showScreen, setShowScreen] = useState(1);
 
-            <TelaLogin />
+    // USANDO SWITCH CASE:
+    switch (showScreen) {
+        case 1:
+            return (
+                <MainContainer>
+                    <GlobalStyled />
+                    <TelaLogin />
+                </MainContainer>
+            );
+        case 2:
+            return (
+                <MainContainer>
+                    <GlobalStyled />
+                    <TelaCadastro />
+                </MainContainer>
+            );
+        case 3:
+            return (
+                <MainContainer>
+                    <GlobalStyled />
+                    <TelaPrincipal />
+                </MainContainer>
+            );
+        default:
+            return 'Página não encontrada';
+    }
 
-            <TelaCadastro />
+    // USANDO IF/ELSE:
+    // if (showScreen === 1) {
+    //     return (
+    //         <MainContainer>
+    //             <GlobalStyled />
+    //             <TelaLogin />
+    //         </MainContainer>
+    //     );
+    // } else if (showScreen === 2) {
+    //     return (
+    //         <MainContainer>
+    //             <GlobalStyled />
+    //             <TelaCadastro />
+    //         </MainContainer>
+    //     );
+    // } else if (showScreen === 3) {
+    //     return (
+    //         <MainContainer>
+    //             <GlobalStyled />
+    //             <TelaPrincipal />
+    //         </MainContainer>
+    //     );
+    // } else {
+    //     return 'Página não encontrada';
+    // }
 
-            <TelaPrincipal />
-        </MainContainer>
-    );
+    // USANDO TERNÁRIO:
+    // return (
+    //     <>
+    //         {showScreen === 1 ? (
+    //             <MainContainer>
+    //                 <GlobalStyled />
+    //                 <TelaLogin />
+    //             </MainContainer>
+    //         ) : (
+    //             ''
+    //         )}
+    //         {showScreen === 2 ? (
+    //             <MainContainer>
+    //                 <GlobalStyled />
+    //                 <TelaCadastro />
+    //             </MainContainer>
+    //         ) : (
+    //             ''
+    //         )}
+    //         {showScreen === 3 ? (
+    //             <MainContainer>
+    //                 <GlobalStyled />
+    //                 <TelaPrincipal />
+    //             </MainContainer>
+    //         ) : (
+    //             'Página não encontrada'
+    //         )}
+    //     </>
+    // );
 }
 
 export default App;
